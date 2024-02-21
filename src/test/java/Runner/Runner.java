400d04013a2f74a398787e98e859a14af48f1c8e
muhammadali426 <73887227+muhammadali426@users.noreply.github.com>
2024-02-21 21:35:48 +0500

    Update Runner.java

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -20,11 +20,11 @@ dryRun = false,
 monochrome = true,
 //plugin = {"pretty" , "html: test-output"},
 
-//plugin = {"pretty", "json:target/cucumber-reports/cucumber.json",
-//		"html:target/cucumber-reports/cucumber.html", }
+plugin = {"pretty", "json:target/cucumber-reports/cucumber.json",
+		"html:target/cucumber-reports/cucumber.html", }
 
-		plugin = {"pretty", "json:report/cucumber.json",
-				"html:report/cucumber.html", }
+//		plugin = {"pretty", "json:report/cucumber.json",
+//				"html:report/cucumber.html", }
 
 		)
 public class Runner {

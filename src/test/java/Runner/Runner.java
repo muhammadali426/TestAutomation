872a3c43bb867eecdfe17b56cceb1c99df2e4dd4
muhammadali426 <73887227+muhammadali426@users.noreply.github.com>
2024-02-21 22:20:53 +0500

    Update Runner.java

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -9,23 +9,11 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features="src/test/java/formfeature/form1.feature",
-/*@CucumberOptions(features={"src\\test\\java\\FEATURE\\businessPhone.feature",
-"src\\test\\java\\FEATURE\\videoconferencing.feature","src\\test\\java\\FEATURE\\teamchat.feature",
-"src\\test\\java\\FEATURE\\integration.feature","src\\test\\java\\FEATURE\\contactcenter.feature",
-"src\\test\\java\\FEATURE\\api.feature","src\\test\\java\\FEATURE\\download.feature"},*/
+@CucumberOptions(
+features="src/test/java/formfeature/form1.feature",
 glue="formDefinition",
- tags = "@ValidationTest",
-dryRun = false,
-monochrome = true,
-//plugin = {"pretty" , "html: test-output"},
-
-plugin = {"pretty", "json:target/cucumber-reports/cucumber.json",
-		"html:target/cucumber-reports/cucumber.html", }
-
-//		plugin = {"pretty", "json:report/cucumber.json",
-//				"html:report/cucumber.html", }
-
+tags = "@ValidationTest",
+plugin = {"json:target/cucumber.json", "pretty" }
 		)
 public class Runner {
 
